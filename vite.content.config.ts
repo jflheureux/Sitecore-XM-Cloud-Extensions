6eb431613ebef1b94a862b9a5149829bc2b73ce8
Mihaly Arvai <mihaly_arvai@epam.com>
2023-11-23 09:32:44 +0100

    adding react build

---
+++ vite.content.config.ts
@@ -0,0 +1,25 @@
+import path from 'node:path'
+
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  plugins: [],
+  define: {
+    'process.env': {}
+  },
+  build: {
+    emptyOutDir: false,
+    outDir: path.resolve(__dirname, 'dist'),
+    lib: {
+      formats: ['iife'],
+      entry: path.resolve(__dirname, 'content-script', 'index.tsx'),
+      name: 'Sitecore XM Cloud Extensions'
+    },
+    rollupOptions: {
+      output: {
+        entryFileNames: 'contentScript/index.js',
+        extend: true,
+      }
+    }
+  }
+})
